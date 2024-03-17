d8ffb2a22a75c9f776e8372254d3925a95eaa143
Mohammed-Umair-Kalim <umairkalim@gmail.com>
2024-03-17 00:21:40 -0700

    A simple calculator with Typescript,Node.js,Inquirer and Chalk

---
+++ simple-calculator-umair/simple_calculator.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let simple_calculator = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "Enter first number: "
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "Enter operator: ",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter second number: "
+    }
+]);
+//conditional statements:
+if (simple_calculator.operator === "Addition") {
+    console.log(chalk.yellow.bgRed.bold.italic("Addition:", simple_calculator.num1 + simple_calculator.num2));
+}
+else if (simple_calculator.operator === "Subtraction") {
+    console.log(chalk.magentaBright.bgWhite.bold.italic("Subtraction:", simple_calculator.num1 - simple_calculator.num2));
+}
+else if (simple_calculator.operator === "Multiplication") {
+    console.log(chalk.cyan.bgMagenta.bold.italic("Multiplication:", simple_calculator.num1 * simple_calculator.num2));
+}
+else if (simple_calculator.operator === "Division") {
+    console.log(chalk.green.bgBlue.bold.italic("Division:", simple_calculator.num1 / simple_calculator.num2));
+}
+else {
+    console.log("Please select a valid operator!");
+}
